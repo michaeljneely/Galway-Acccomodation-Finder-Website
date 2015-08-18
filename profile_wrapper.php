394a1ca092a01d838814a5e839ee2396fd295d14
Michael Neely <michaeljneelysd@gmail.com>
2015-08-17 18:11:59 -0700

    Create profile_wrapper.php

---
+++ profile_wrapper.php
@@ -0,0 +1,21 @@
+<?php 
+/*
+	Side banner of account page
+*/
+
+echo '
+	<div class="col-sm-3">
+      <!-- left -->
+      <h3><i class="glyphicon glyphicon-wrench"></i> Options</h3>
+      <hr>
+      <ul class="nav nav-stacked">
+		<li><a href="profile.php"><i class="glyphicon glyphicon-star"></i> My Posts</a></li>
+        <li><a href="upload.php"><i class="glyphicon glyphicon-circle-arrow-up"></i> Upload a Post</a></li>
+        <li><a href="details.php"><i class="glyphicon glyphicon-list-alt"></i> Account Details</a></li>
+        <li><a href="logout_parse.php"><i class="glyphicon glyphicon-off"></i> Logout</a></li>
+      </ul>
+      <hr>
+	</div>
+	<div class="col-sm-9 text-center">
+      ';
+?>
